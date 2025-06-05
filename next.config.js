32ebca73598f26931ea7a7d3c6ff43b40b41b687
amadyira25@amherst.edu <amadyira25@amherst.edu>
2025-06-04 20:29:20 -0400

    Rebuild...

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 // next.config.js
-const isGithubPages = process.env.NODE_ENV === 'production';
 
 module.exports = {
   output: 'export',
-  basePath: isGithubPages ? '/AspireUS' : '',
-  assetPrefix: isGithubPages ? '/AspireUS/' : '',
+  basePath: '',
+  assetPrefix: '',
 };
